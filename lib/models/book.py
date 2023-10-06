db80eb313720570ad629b0789bfd2cff3946591a
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-10-05 22:06:39 -0400

    book title setter complete

--- lib/models/book.py
+++ lib/models/book.py
@@ -17,3 +17,16 @@ class Book:
             f"<<<Book {self.id}: {self.title}, {self.total_pages}, {self.rating}, {self.published_date}, " +
             f"Author ID: {self.author_id} >>>"
         )
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and len(title):
+            self._title = title
+        else:
+            raise ValueError(
+                "Book title must be a non-empty string"
+            )
