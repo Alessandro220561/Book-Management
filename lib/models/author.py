940f1b12b3ec6c37fd5187c33fd8eccfa9f428fa
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-10-05 21:34:28 -0400

    author class started

--- lib/models/author.py
+++ lib/models/author.py
@@ -1,5 +1,6 @@
 from models.__init__ import CURSOR, CONN
 
+
 class Author:
 
     all_authors = {}
@@ -10,4 +11,16 @@ class Author:
 
     def __repr__(self):
         return f"<<<Author {self.id}: {self.name}>>>"
-        
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "<<< Author's name must be a non-empty string >>>"
+            )
